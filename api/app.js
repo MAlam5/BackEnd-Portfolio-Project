e25cb08df1026205dbcfb47631ecd85ef6d4db98
MAlam5 <mohammedhassan9445@gmail.com>
2022-09-05 15:57:35 +0100

    have handled errors and shortened by import for test data

--- api/app.js
+++ api/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
-const { getTopics } = require('./controllers/api.topics.contoller')
+const { getTopics, getArticleById } = require('./controllers/api.topics.contoller')
+const { handleServerError } = require('./error_handlers/errors')
 
 const app = express()
 
 app.get('/api/topics', getTopics)
 
+app.get('/api/articles/:article_id',getArticleById)
+
+app.use(handleServerError)
 module.exports = app
