3669e3a6b301a89e1bb4faef6ea5df54cecb1139
MAlam5 <mohammedhassan9445@gmail.com>
2022-09-06 14:59:44 +0100

    have added patch func with errors handled

--- api/controllers/api.articles.controller.js
+++ api/controllers/api.articles.controller.js
@@ -1,17 +1,19 @@
-const { fetchArticlebyId } = require("../models/api.articles.models");
+const users = require("../../db/data/test-data/users");
+const { fetchArticlebyId, updateArticle } = require("../models/api.articles.models");
 
 exports.getArticleById = (req, res, next) => {
-    const { article_id } = req.params;
-    return fetchArticlebyId(article_id)
-      .then((article) => {
-        res.status(200).send({ article });
-      })
-      .catch(next);
-  };
+  const { article_id } = req.params;
+  return fetchArticlebyId(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch(next);
+};
 
-  exports.patchArticle=()=>{
-    const articleId = req.params.article_id
-    const { body }= req
-    updateArticle(articleId, body )
-  }
-  
+exports.patchArticle = (req, res, next) => {
+  const articleId = req.params.article_id;
+  const { body } = req;
+  updateArticle(articleId, body).then((article) => {
+    res.status(200).send({ article });
+  }).catch(next)
+};
--- api/models/api.articles.models.js
+++ api/models/api.articles.models.js
@@ -8,3 +8,20 @@ exports.fetchArticlebyId = (article_id) => {
       return article.rows[0];
     });
 };
+
+exports.updateArticle = (articleId, body)=>{
+  console.log(body);
+
+  if(Object.keys(body).length !== 1 && !body.hasOwnProperty('inc_votes')){
+    return Promise.return({status: 400 , msg: 'bad request'})
+  }
+  const newVotes = body.inc_votes
+  return db.query('UPDATE articles SET votes = $1 WHERE article_id = $2 RETURNING *', [newVotes,articleId])
+  
+  .then(article=>{
+    if(article.rows.length === 0){
+      return Promise.reject({status:404 , msg: 'not found'})
+    }
+    return article.rows[0]
+  })
+}
