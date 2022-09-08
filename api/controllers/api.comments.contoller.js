e75303a9d5fba609e91a3df8df16d411cdb98f1e
MAlam5 <mohammedhassan9445@gmail.com>
2022-09-08 10:48:30 +0100

    neater heandling of res send for .delete

--- api/controllers/api.comments.contoller.js
+++ api/controllers/api.comments.contoller.js
@@ -3,6 +3,6 @@ const { removeCommentById } = require("../models/api.comments.model")
 exports.deleteCommentById = (req,res,next)=>{
     const commentId = req.params.comment_id
     return removeCommentById(commentId).then(()=>{
-        res.status(204).send()
+        res.sendStatus(204)
     }).catch(next)
 }
